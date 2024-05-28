8305542732e5dda2859379ee433341d0bcabbce7
Tun Lin <lintun2003@gmail.com>
2024-05-28 16:20:13 -0400

    setting up files and buttons

---
+++ script.js
@@ -0,0 +1,23 @@
+const a = 0;
+const b = 0;
+const c = 0;
+
+function add(a,b) {
+
+}
+
+function subtract(a,b) {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function operate(a,b,c) {
+
+}
