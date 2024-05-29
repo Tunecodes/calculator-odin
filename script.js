b5aecaf3977e29bba18eaa8c41d087085ee84e9f
Tun Lin <lintun2003@gmail.com>
2024-05-29 18:21:23 -0400

    added adition operation

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
-const a = 0;
-const b = 0;
-const c = 0;
+const button = document.querySelectorAll("button");
+const dis = document.getElementById("display");
+let a = null;
+let b = null;
+let opp = null;
+dis.textContent = null;
+button.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.classList.contains("number")) {
 
-function add(a,b) {
 
+    
+
+
+      dis.textContent += button.value;
+    } else if (button.classList.contains("operator")) {
+      let num = dis.textContent;
+      let operator = button.value;
+      a = num;
+      opp = operator;
+      console.log(a);
+      console.log(opp);
+      dis.textContent = null;
+    } else if (button.classList.contains("equal")) {
+        b = dis.textContent;
+        console.log(b);
+        console.log(operator(a,b,opp));
+        dis.textContent = operator(a,b,opp);
+        a = dis.textContent;
+        b = null;
+
+
+
+    }
+  });
+});
+
+function operator(a,b,opp) {
+    if (opp === "+")
+    return parseInt(a) + parseInt(b);
 }
 
-function subtract(a,b) {
-
+function updateNum (num, operator) {
+    if (a == null) {
+        a = num;
+    } else {
+        b = num;
+    }
+    opp = operator;
 }
 
-function multiply() {
+function display(button) {}
 
-}
+function add(a, b) {}
 
-function divide() {
+function subtract(a, b) {}
 
-}
+function multiply() {}
 
-function operate(a,b,c) {
+function divide() {}
 
-}
+
