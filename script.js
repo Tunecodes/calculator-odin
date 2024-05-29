39458d7c4c51f5d310b09ed56b39b92ee12c8bc1
Tun Lin <lintun2003@gmail.com>
2024-05-29 19:15:14 -0400

    added all the operators

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const button = document.querySelectorAll("button");
 const dis = document.getElementById("display");
 let a = null;
 let b = null;
+let c = null;
 let opp = null;
 dis.textContent = null;
 button.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("number")) {
-
-
-    
-
-
+      if (c != null) {
+        dis.textContent = null;
+        c = null;
+      }
       dis.textContent += button.value;
     } else if (button.classList.contains("operator")) {
       let num = dis.textContent;
@@ -22,41 +22,37 @@ button.forEach((button) => {
       console.log(opp);
       dis.textContent = null;
     } else if (button.classList.contains("equal")) {
-        b = dis.textContent;
-        console.log(b);
-        console.log(operator(a,b,opp));
-        dis.textContent = operator(a,b,opp);
-        a = dis.textContent;
-        b = null;
-
-
-
+      b = dis.textContent;
+      console.log(b);
+      console.log(operator(a, b, opp));
+      dis.textContent = operator(a, b, opp);
+      a = dis.textContent;
+      c = dis.textContent;
+      b = null;
     }
   });
 });
 
-function operator(a,b,opp) {
-    if (opp === "+")
-    return parseInt(a) + parseInt(b);
+function operator(a, b, opp) {
+  switch (opp) {
+    case "+":
+      return parseFloat(a) + parseFloat(b);
+    case "-":
+      return parseFloat(a) - parseFloat(b);
+    case "*":
+      return parseFloat(a) * parseFloat(b);
+    case "/":
+      return parseFloat(a) / parseFloat(b);
+  }
 }
 
-function updateNum (num, operator) {
-    if (a == null) {
-        a = num;
-    } else {
-        b = num;
-    }
-    opp = operator;
+function updateNum(num, operator) {
+  if (a == null) {
+    a = num;
+  } else {
+    b = num;
+  }
+  opp = operator;
 }
-
-function display(button) {}
-
-function add(a, b) {}
-
-function subtract(a, b) {}
-
-function multiply() {}
-
-function divide() {}
 
 
